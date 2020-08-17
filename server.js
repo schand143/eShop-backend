a471ee0df40d8f7b8516421de8e8b7b6f21a388e
schand143 <shaikhchand724@gmail.com>
2020-08-17 18:30:59 +0530

    port change

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require('graphql-yoga');
 
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const Product = require('./resolvers/Product')
-const Brand = require('./resolvers/Brand')
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const Product = require('./resolvers/Product');
+const Brand = require('./resolvers/Brand');
 
 const resolvers = {
-    Query,
-    Mutation,
-    Product,
-    Brand
-}
+  Query,
+  Mutation,
+  Product,
+  Brand,
+};
 
 const server = new GraphQLServer({
-    typeDefs: './types/schema.graphql',
-    resolvers
-})
-server.start(() => console.log('Server listening at 4000'))
+  typeDefs: './types/schema.graphql',
+  resolvers,
+});
+
+// const PORT = process.env.PORT || 4000;
+
+// server.listen({ PORT }).then(({ url }) => {
+//   console.log(`Server is ready at ${url}`);
+// });
+
+server.start(() => console.log('Server listening at 4000'));
