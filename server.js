b15b4e9811a3d95108852f78521eb2b1b9a23b63
schand143 <shaikhchand724@gmail.com>
2020-08-17 21:18:21 +0530

    erro solve

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const Product = require('./resolvers/Product');
 const Brand = require('./resolvers/Brand');
+var express = require('express');
 
 const resolvers = {
   Query,
@@ -17,7 +18,16 @@ const server = new GraphQLServer({
   resolvers,
 });
 
-server.start(() => console.log('Server listening at 4000'));
+// var server = express();
+// server.listen(process.env.PORT || 4000);
+
+// server.start(() => console.log('Server listening at 4000'));
 
 // server.listen(3001, () => {});
 // console.log('Node server running on port 3000');
+
+// var port = process.env.port || 4000;
+
+// server.listen(port);
+
+server.start(4000, () => console.log('Server running on port 4000'));
